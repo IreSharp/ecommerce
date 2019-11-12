1120f965f4bbe37e3cb50123079580efbcfa1dcb
Romulo Lobosco <rlobosco_prest@telecine.com.br>
2019-11-11 21:49:19 -0300

    create relations between item and category

--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
     
 class Item(models.Model):
     name = models.CharField(max_length = 100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
     description = models.CharField(max_length = 255, blank = True, null = True)
 
     def __str__(self):
