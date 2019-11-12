88190da19a89759409f2470cba8472858fdbc551
Romulo Lobosco <rlobosco_prest@telecine.com.br>
2019-11-11 21:16:03 -0300

    adding category

--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
 class Item(models.Model):
     name = models.CharField(max_length = 100)
     description = models.CharField(max_length = 255, blank = True, null = True)
 
     def __str__(self):
         return self.name
+
     
