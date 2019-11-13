46ed489f78ba6e072df299b8e2c0462fd3a977a0
Romulo Lobosco <rlobosco_prest@telecine.com.br>
2019-11-12 22:55:50 -0300

    registering Order and OrderItem on admin

--- ecommerce/core/admin.py
+++ ecommerce/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, Category
+from .models import Item, Category, Order, OrderItem
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
@@ -8,3 +8,13 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        print('obj variable')
+        obj.item_price = Item.objects.get(pk = obj.item.id).category.price
+        obj.item_description = Item.objects.get(pk = obj.item.id).description
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Order)
